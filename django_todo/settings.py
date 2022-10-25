24e4cf6d0089bcc8da8b9e8b2ca1decd47d2df8d
Henry Leon <henryatusabes@gmail.com>
2022-10-25 20:09:10 +0000

    Update settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -90,16 +90,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 #     }
 # }
 
-if development:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
 
 
