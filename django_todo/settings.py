be1065e0d5c4b1a603f7426149732400b4a50234
Henry Leon <henryatusabes@gmail.com>
2022-10-25 19:43:42 +0000

    Update settings.py file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+# DEBUG = development
+DEBUG = 'DEVELOPMENT' in os.environ
 
 if development:
     ALLOWED_HOSTS = ['localhost']
